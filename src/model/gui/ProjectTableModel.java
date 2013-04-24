53672de8c4787bb5ade3b39417262d007910f08d
7SLEVIN <mafaer@gmail.com>
2013-04-24 15:00:56 +0200

    Updated gui

--- src/model/gui/ProjectTableModel.java
+++ src/model/gui/ProjectTableModel.java
@@ -30,4 +30,6 @@ public class ProjectTableModel extends DefaultTableModel implements TableModel {
 			default: return null;
 		}
 	}
+	
+	
 }
--- src/view/state/MenuViewState.java
+++ src/view/state/MenuViewState.java
@@ -1,21 +1,33 @@
 package view.state;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class MenuViewState extends AbstractViewState {
 	private JButton projectsButton;
 	private JButton developersButton;
 	private JButton calendarButton;
+	private JList favorites;
 	
 	public MenuViewState() {
 		this.projectsButton = new JButton("Projects");
 		this.developersButton = new JButton("Developers");
 		this.calendarButton = new JButton("Register time");
+		
+		JPanel favoritesPanel = new JPanel();
+		this.favorites = new JList();
+		this.favorites.add(new JLabel("Hello"));
+		this.favorites.add(new JLabel("Hello"));
+		favoritesPanel.add(new JLabel("My favorites:"));
+		favoritesPanel.add(this.favorites);
 
 		this.add(this.calendarButton);
 		this.add(this.developersButton);
 		this.add(this.projectsButton);
+		this.add(favoritesPanel);
 	}
 
 	public JButton getDevelopersButton() {
