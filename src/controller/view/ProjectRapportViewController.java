9a9ab9e5a96bdebf8be470d90e88f8fce16ecc6d
7SLEVIN <mafaer@gmail.com>
2013-05-06 16:25:41 +0200

    Gui touchup

--- src/controller/view/ProjectRapportViewController.java
+++ src/controller/view/ProjectRapportViewController.java
@@ -1,5 +1,10 @@
 package controller.view;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Activity;
+import model.ActivityDeveloperRelation;
 import model.Project;
 import persistency.Database;
 import view.ViewContainer;
@@ -33,6 +38,17 @@ public class ProjectRapportViewController extends AbstractViewController {
 		
 		this.viewState.getBackButton().addActionListener(new ChangeViewAction(this.viewContainer, ViewControllerFactory.CreateProjectMaintainanceViewController(this.project.getId())));
 		
+		this.setTimeUsed();
+	}
+	
+	private void setTimeUsed() {
+		long timeUsed = 0;
+		for (Activity activity : this.project.getActivities()) {
+			List<ActivityDeveloperRelation> rel = new ArrayList<ActivityDeveloperRelation>();
+//			this.database.registerTime().readAllWhereEquals(, value);
+			
+		}
+		this.viewState.setTimeUsed(timeUsed);
 	}
 
 }
