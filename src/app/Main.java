3294dc6bd979e1060adbc5ab018b371b3ed1bc39
patrickgadd <patrick_gadd@hotmail.com>
2013-05-06 19:11:25 +0200

    deadline in project is now actually a long

--- src/app/Main.java
+++ src/app/Main.java
@@ -11,7 +11,6 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Database database = new Database("dev_db.db");
-		
 		// Wire up
 		TimeService timeService = new TimeService();
 		
