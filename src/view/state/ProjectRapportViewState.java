d635790fd33149b889626fd0121d35dd6256ae7d
patrickgadd <patrick_gadd@hotmail.com>
2013-05-10 20:33:19 +0200

    initialized two use-cases

--- src/view/state/ProjectRapportViewState.java
+++ src/view/state/ProjectRapportViewState.java
@@ -22,7 +22,7 @@ public class ProjectRapportViewState extends AbstractViewState {
 	private JButton backButton;
 
 	private long timeUsed;
-	private long timeExpected;
+	private long timeAssignedToActivities;
 	
 	private JLabel nameLabel;
 	private JLabel activityCountLabel;
@@ -39,16 +39,13 @@ public class ProjectRapportViewState extends AbstractViewState {
 		this.backButton = new JButton("Back to project");
 		this.nameLabel = new JLabel(projectName);
 		this.nameLabel.setFont(new Font("DejaVu Sans", Font.PLAIN, 20));
-//<<<<<<< HEAD
 		this.activityCountLabel = new JLabel("Number of activities: " + String.valueOf(noOfActivities));
 		this.timeUsedLabel = new JLabel();
 		this.estimatedPercentageComplete = new JLabel();
 		this.estimatedTimeRemaining = new JLabel();
-//=======
 		this.timeExpectedLabel = new JLabel();
 		this.timeUsedLabel = new JLabel();
 		this.timeLeftLabel = new JLabel();
-//>>>>>>> d8732b6f51145e3757e2f8a732ca594f67f08503
 		
 		JPanel titlePanel = new JPanel();
 		GuiUtils.setSize(titlePanel, new Dimension(ViewContainer.WINDOW_WIDTH, 30));
@@ -60,19 +57,14 @@ public class ProjectRapportViewState extends AbstractViewState {
 		infoPanel.add(this.activityCountLabel);
 		infoPanel.add(this.timeExpectedLabel);
 		infoPanel.add(this.timeUsedLabel);
-//<<<<<<< HEAD
 		infoPanel.add(this.estimatedPercentageComplete);
 		infoPanel.add(this.estimatedTimeRemaining);
-		
-//=======
 		infoPanel.add(this.timeLeftLabel);
-//>>>>>>> d8732b6f51145e3757e2f8a732ca594f67f08503
 		
 		this.add(titlePanel);
 		this.add(infoPanel);
 	}
 	
-//<<<<<<< HEAD
 	public void setChart(int remainingTime, int registeredTime){
 		DefaultPieDataset data = new DefaultPieDataset();
 		data.setValue("Remaining time", remainingTime);
@@ -90,28 +82,31 @@ public class ProjectRapportViewState extends AbstractViewState {
 	}
 	
 	public void setTimeUsed(int timeUsed) {
-		this.timeUsedLabel.setText("Total time used: " + String.valueOf(timeUsed) + (timeUsed == 1 ? "hour" :" hours"));
+		this.timeUsed = timeUsed;
+		this.timeUsedLabel.setText("Total time registered1: " + String.valueOf(timeUsed) + (timeUsed == 1 ? "hour" :" hours"));
 	}
 	
 	public void setTimeRemaining(int timeRemaining){
+		if (timeRemaining < 0) this.estimatedTimeRemaining.setForeground(Color.RED);
 		this.estimatedTimeRemaining.setText("Estimated time remaining: " + String.valueOf(timeRemaining) + " hours");
 	}
-//=======
-	public void setTimeUsed(long timeUsed) {
-		this.timeUsed = timeUsed;
-		this.timeUsedLabel.setText(String.format("Total time used: %d min.", timeUsed));
+
+	//TODO percentage of budgeted time assigned to activities
+
+	public void setTimeExpected(long timeAssignedToActivities) {
+		this.timeAssignedToActivities = timeAssignedToActivities;
+		this.timeExpectedLabel.setText(String.format("Time assigned to activities: %d hours", timeAssignedToActivities/60));
 	}
-	
-	public void setTimeExpected(long timeExpected) {
-		this.timeExpected = timeExpected;
-		this.timeExpectedLabel.setText(String.format("Total expected time: %d min.", timeExpected));
-	}
-	
+
 	public void setTimeLeft() {
-		long timeLeft = this.timeExpected - this.timeUsed;
-		if (timeLeft < 0) this.timeLeftLabel.setForeground(Color.RED);
-		this.timeLeftLabel.setText(String.format("Time left: %d min.", timeLeft));
-//>>>>>>> d8732b6f51145e3757e2f8a732ca594f67f08503
+		//TODO delete this
+//		long timeLeft = this.timeAssignedToActivities - this.timeUsed;
+//		
+//		this.timeLeftLabel.setText(String.format("Time left: %d hours", timeLeft/60));
+	}
+	
+	public void percentageUnassigned(){
+		this.timeLeftLabel.setText("skriv info om hvor mange procent af budgeted hours der er delt ud på aktiviteter");
 	}
 	
 	public JButton getBackButton() {
